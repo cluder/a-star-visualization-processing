8210448cce0e3186386ef14ce80001cd8289e490
cluder <christoph.luder@gmail.com>
2019-03-23 21:43:13 +0100

    added interactive mode and on screen help
    - 'p'    - pause/unpause calculation
    - space - reset field
    - 'r'   - block random cells
    - 'b'   - block all cells
    - mouse - block unblock cells with mouse

--- a-star-processing/src/ch/coredump/astar/AStarMain.java
+++ a-star-processing/src/ch/coredump/astar/AStarMain.java
@@ -11,8 +11,13 @@ import processing.core.PApplet;
  * https://www.youtube.com/watch?v=aKYlikFAV4k
  */
 public class AStarMain extends PApplet {
-	static final int ROWS = 22;
-	static final int COLS = 22;
+	static final int ROWS = 20;
+	static final int COLS = 20;
+	final int SCHREEN_WIDTH = 800;
+	final int SCREEN_HEIGHT = 600;
+
+	int cellWidth = SCHREEN_WIDTH / COLS;
+	int cellHeight = SCREEN_HEIGHT / ROWS;
 
 	boolean diagonalMoveAllowed = true;
 	float blockChance = 0.4f; // 0.0 - 1.0
@@ -26,13 +31,16 @@ public class AStarMain extends PApplet {
 	Node start;
 	Node end;
 
+	boolean pause = false;
+	private String infoText = "";
+
 	public static void main(String[] args) {
 		PApplet.main(AStarMain.class, args);
 	}
 
 	@Override
 	public void settings() {
-		size(800, 600);
+		size(SCHREEN_WIDTH, SCREEN_HEIGHT, JAVA2D);
 	}
 
 	@Override
@@ -42,34 +50,52 @@ public class AStarMain extends PApplet {
 		initialize();
 	}
 
+	private void reset() {
+		pause = true;
+		openSet.clear();
+		closedSet.clear();
+		solution.clear();
+
+		// initialize the open set (closed set is empty)
+		openSet.add(start);
+		setInfoText("");
+	}
+
 	private void initialize() {
-		openSet = new ArrayList<>();
-		closedSet = new ArrayList<>();
-		solution = new ArrayList<>();
+		pause = true;
 
 		// initialize grid
-		grid = new Node[ROWS][COLS];
-		for (int row = 0; row < ROWS; row++) {
-			for (int col = 0; col < COLS; col++) {
+		grid = new Node[COLS][ROWS];
+		for (int col = 0; col < COLS; col++) {
+			for (int row = 0; row < ROWS; row++) {
 				// create Nodes and set positions
-				grid[row][col] = new Node(row, col, (col + 0.5f) * width / COLS, ((row + 0.5f) * height / ROWS));
-				if (random(1) < blockChance) {
-					grid[row][col].blocked = true;
-				}
+				final Node n = grid[col][row];
+				grid[col][row] = new Node(row, col, (col + 0.5f) * width / COLS, ((row + 0.5f) * height / ROWS));
 			}
 		}
 
 		// initialize start / end
 		start = grid[0][0];
-		end = grid[ROWS - 1][COLS - 1];
-
-		start.blocked = false;
-		end.blocked = false;
+		end = grid[COLS - 1][ROWS - 1];
 
 		// initialize the open set (closed set is empty)
 		openSet.add(start);
+	}
 
-		loop();
+	private void blockRandomNodes() {
+		for (int col = 0; col < COLS; col++) {
+			for (int row = 0; row < ROWS; row++) {
+				final Node node = grid[col][row];
+				node.blocked = false;
+				// block random nodes
+				if (random(1) < blockChance) {
+					node.blocked = true;
+				}
+			}
+		}
+
+		start.blocked = false;
+		end.blocked = false;
 	}
 
 	/**
@@ -79,9 +105,9 @@ public class AStarMain extends PApplet {
 	List<Node> getNeighbors(int r, int c) {
 		List<Node> neighbors = new ArrayList<>();
 
-		for (int row = max(r - 1, 0); row <= min(r + 1, ROWS - 1); row++) {
-			for (int col = max(c - 1, 0); col <= min(c + 1, COLS - 1); col++) {
-				final Node n = grid[row][col];
+		for (int col = max(c - 1, 0); col <= min(c + 1, COLS - 1); col++) {
+			for (int row = max(r - 1, 0); row <= min(r + 1, ROWS - 1); row++) {
+				final Node n = grid[col][row];
 				if (n.blocked) {
 					continue;
 				}
@@ -105,18 +131,98 @@ public class AStarMain extends PApplet {
 	 */
 	@Override
 	public void draw() {
-		boolean noSolution = false;
+		diagonalMoveAllowed = true;
 		background(0);
+		frameRate(15);
 
-		if (openSet.isEmpty() == false) {
+		if (pause == false) {
+			setInfoText("");
+			findPath();
+		}
+
+		// draw start / end
+		fill(Color.YELLOW.getRGB());
+		ellipse(start.posX, start.posY, 10, 10);
+		ellipse(end.posX, end.posY, 10, 10);
+
+		// draw all nodes
+		for (int col = 0; col < COLS; col++) {
+			for (int row = 0; row < ROWS; row++) {
+				final Node n = grid[col][row];
+				if (openSet.contains(n) || closedSet.contains(n)) {
+					continue;
+				}
+				n.draw(this, new Color(250, 250, 250, 100));
+			}
+		}
+
+		// draw open / closed sets
+		for (Node n : openSet) {
+			n.draw(this, new Color(50, 50, 255));
+		}
+		for (Node n : closedSet) {
+			n.draw(this, new Color(255, 50, 50));
+		}
+
+		// draw solution
+		drawSolution();
+
+		drawText();
+	}
+
+	private void drawText() {
+		if (pause == false) {
+			return;
+		}
+
+		// text background
+		stroke(255, 255, 0, 100);
+		rectMode(CORNER);
+		fill(0, 180);
+		rect(10, height * 0.76f, 270, 130);
+
+		textSize(15);
+		fill(255, 200, 0, 250);
+		textAlign(LEFT);
+		text("SPACE - reset\n"//
+				+ "P - toggle PAUSE\n" //
+				+ "B - block/unblock all cells\n" //
+				+ "R - block random cells\n" //
+				+ "Mouse Btn - block/unblock cells\n", //
+				20, height * 0.8f);
+
+		stroke(255, 255, 0, 100);
+		rectMode(CENTER);
+		fill(0, 150);
+		rect(width / 2, 50, 250, 65);
+
+		textAlign(CENTER, CENTER);
+		textSize(25);
+		fill(255, 200, 0, 250);
+		text(infoText, width / 2, height * 0.05f);
+		text("paused", width / 2, height * 0.1f);
+	}
+
+	/**
+	 * returns false, if no solution possible. <br>
+	 * returns true if solution is found or not done yet.
+	 */
+	private boolean findPath() {
+		if (openSet.isEmpty()) {
+			// no possible solution
+			pause = true;
+			return false;
+		} else {
 			// Nodes in open set have to be checked
 			// find node with lowest f-score
 			Node current = getLowestFScore(openSet);
 
 			if (current == end) {
 				// finished
-				System.out.println("DONE");
-				noLoop();
+				// add end node to solution
+				trackSolution(current);
+				pause = true;
+				return true;
 			}
 
 			// add this node with the lowest f-score to the closed set
@@ -154,45 +260,21 @@ public class AStarMain extends PApplet {
 			}
 
 			// track current solution
-			if (noSolution == false) {
-				solution.clear();
-				Node n = current;
-				while (n.previous != null) {
-					solution.add(n);
-					n = n.previous;
-				}
-				solution.add(start);
-			}
-
-		} else {
-			// openset is empty - finished / no solution
-			System.out.println("openset empty - no solution found");
-			noSolution = true;
-			noLoop();
+			trackSolution(current);
 		}
 
-		// draw nodes
-		for (int row = 0; row < ROWS; row++) {
-			for (int col = 0; col < COLS; col++) {
-				grid[row][col].draw(this, new Color(250, 250, 250, 20));
-			}
-		}
+		// openset is empty - finished / no solution yet
+		return true;
+	}
 
-		// draw start
-		fill(Color.YELLOW.getRGB());
-		ellipse(start.posX, start.posY, start.size, start.size);
-		ellipse(end.posX, end.posY, end.size, end.size);
-
-		// draw open / closed sets
-		for (Node n : openSet) {
-			n.draw(this, new Color(0, 120, 255));
+	private void trackSolution(Node current) {
+		solution.clear();
+		Node n = current;
+		while (n.previous != null) {
+			solution.add(n);
+			n = n.previous;
 		}
-		for (Node n : closedSet) {
-			n.draw(this, Color.RED);
-		}
-
-		// draw solution
-		drawSolution();
+		solution.add(start);
 	}
 
 	private Node getLowestFScore(List<Node> list) {
@@ -220,8 +302,12 @@ public class AStarMain extends PApplet {
 			textSize(50);
 			textAlign(CENTER, CENTER);
 			stroke(255, 100);
-			text("No solution found", width / 2, height / 2);
+			setInfoText("No solution found");
 		}
+	}
+
+	private void setInfoText(String string) {
+		infoText = string;
 	}
 
 	/**
@@ -240,11 +326,89 @@ public class AStarMain extends PApplet {
 
 	@Override
 	public void keyPressed() {
-		if (key != ' ') {
+		if (key == 'P' || key == 'p') {
+			pause = !pause;
+		}
+		if (key == ' ') {
+			// reset everything
+			reset();
+		}
+		if (key == 'B' || key == 'b') {
+			reset();
+			blockAll();
+		}
+		if (key == 'r' || key == 'R') {
+			reset();
+			blockRandomNodes();
+		}
+	}
+
+	private void blockAll() {
+		boolean toBlock = !grid[1][1].blocked;
+		for (int col = 0; col < COLS; col++) {
+			for (int row = 0; row < ROWS; row++) {
+				final Node node = grid[col][row];
+
+				if (node == start || node == end) {
+					continue;
+				}
+				node.blocked = toBlock;
+			}
+		}
+	}
+
+	@Override
+	public void mouseDragged() {
+		blockUnblockSelectedNode();
+	}
+
+	@Override
+	public void mouseClicked() {
+		blockUnblockSelectedNode();
+	}
+
+	private void blockUnblockSelectedNode() {
+		Node n = getSelectedNode();
+		if (n == null) {
 			return;
 		}
-		// reset everything
-		noLoop();
-		initialize();
+		if (mouseButton == LEFT) {
+			n.blocked = true;
+		} else {
+			n.blocked = false;
+		}
+	}
+
+	@Override
+	public void mouseMoved() {
+		Node n = getSelectedNode();
+		if (n == null) {
+			return;
+		}
+		strokeWeight(1);
+		noFill();
+		stroke(255, 255, 0, 255);
+		rectMode(CENTER);
+		rect(n.posX, n.posY, cellHeight, cellWidth);
+	}
+
+	/**
+	 * returns the currently selected cell, or null if not allowed.
+	 */
+	private Node getSelectedNode() {
+		final int gridX = mouseX / cellWidth;
+		final int gridY = mouseY / cellHeight;
+
+		if (gridX >= ROWS || gridY >= COLS) {
+			System.out.println(String.format("x:%s, y:%s, gridx:%s, gridy:%s", mouseX, mouseY, gridX, gridY));
+		}
+
+		Node n = grid[min(gridX, COLS - 1)][min(gridY, ROWS - 1)];
+
+		if (openSet.contains(n) || closedSet.contains(n) || n == start || n == end) {
+			// only allow block / onblock if not in use
+			return null;
+		}
+		return n;
 	}
 }
